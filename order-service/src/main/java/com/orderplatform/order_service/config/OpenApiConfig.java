f96292f98d3472150533c5e82e2344234b677c38
bodyaaao0o <bodyatop234@gmail.com>
2026-05-18 16:51:17 +0300

    feat: modify Swagger docs

--- order-service/src/main/java/com/orderplatform/order_service/config/OpenApiConfig.java
+++ order-service/src/main/java/com/orderplatform/order_service/config/OpenApiConfig.java
@@ -2,6 +2,8 @@ package com.orderplatform.order_service.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +13,25 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
 
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Order Service API")
                         .version("1.0")
-                        .description("Order Processing Platform API"));
+                        .description("Order Processing Platform API"))
+
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(securitySchemeName)
+                )
+                .schemaRequirement(
+                        securitySchemeName,
+                        new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                );
     }
 }

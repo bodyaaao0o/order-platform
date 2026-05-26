e610b03d4715ef195cca26022d818ac4c0a737b7
bodyaaao0o <bodyatop234@gmail.com>
2026-05-26 12:21:30 +0300

    feat: add docs for inventory service

--- inventory-service/src/main/java/com/orderplatform/inventory_service/product/ProductController.java
+++ inventory-service/src/main/java/com/orderplatform/inventory_service/product/ProductController.java
@@ -5,34 +5,51 @@ import com.orderplatform.inventory_service.product.dto.CreateProductRequest;
 import com.orderplatform.inventory_service.product.dto.ProductResponse;
 import com.orderplatform.inventory_service.product.dto.UpdateStockRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
+@Tag(
+        name = "Products",
+        description = "Inventory product management API"
+)
 @RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
 
     @GetMapping
+    @Operation(
+            summary = "Get all products"
+    )
     public List<ProductResponse> getAllProducts() {
         return productService.getAllProducts();
     }
 
     @GetMapping("/{sku}")
+    @Operation(
+            summary = "Get product by SKU"
+    )
     public ProductResponse getBySku(@PathVariable String sku) {
         return productService.getBySku(sku);
     }
 
     @PostMapping
+    @Operation(
+            summary = "Create new product"
+    )
     public ProductResponse createProduct(@RequestBody CreateProductRequest request) {
         return productService.createProduct(request);
     }
 
     @PatchMapping("/{sku}/stock")
+    @Operation(
+            summary = "Update product stock"
+    )
     public ProductResponse updateStock(@PathVariable String sku, @RequestBody UpdateStockRequest request) {
         return productService.updateStock(sku, request.stock());
     }
